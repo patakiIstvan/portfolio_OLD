b185690af475b6fb4ac03c60aeb116c944523e6c
Pataki István <patakiistvan1997y@gmail.com>
2021-12-07 03:12:23 +0100

    very basic hero page updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import './home.scss';
+import '../../app.scss'
 import bg from '../../assets/images/heroBg.png';
-// Icons and images
 
 function Home() {
   return (
     // <main style={{ backgroundImage: "url(" + bg + ")" }} class="main section" >
-    <main class="main section" >
+    <main className="main section" >
       <section className="hero">
         <div className="left">
           <h1>Hey, I'm Pisti!</h1>
-          <p>I'm a front-end developer, who is huge science and music enthusiastic</p>
-          <button><a>My Projects</a></button>
+          <p>I'm a front-end developer, a huge science and music enthusiastic.</p>
+          <a>My Projects</a>
         </div>
         <div className="right">
-          <img src="" alt="A pic of Pisti, who is watching deep into your soul." />
+          <div className="imgContainer">
+            <img src="/assets/images/profilePicLong.jpg" alt="A pic of Pisti, who is watching deep into your soul." />
+          </div>
         </div>
       </section>
     </main >
