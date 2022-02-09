af8e88a2b0d37f14cc22e95b7bf14f7e6afddabf
Pataki István <patakiistvan1997y@gmail.com>
2022-02-09 22:16:23 +0100

    database for filter

---
+++ src/projects.js
@@ -0,0 +1,12 @@
+export default [
+  [ {title: "Portfolió",
+     tech: ["HTML","CSS/SCSS","javascript","react.js"],
+     pic: "",
+     category: ["mydesign"]
+    },
+    {title: "Kártyák",
+     tech: ["HTML","CSS/PostCSS","javascript"],
+     pic: "",
+     category: ["notmydesign"]
+    }
+]
