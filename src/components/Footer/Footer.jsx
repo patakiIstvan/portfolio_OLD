73a119353358d91d48ece0f0d4551a98c2b4a1df
Pataki István <patakiistvan1997y@gmail.com>
2022-02-14 02:59:41 +0100

    Added temporal footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 function Footer() {
   return (
     <footer id="footer" class="footer section">
+      <div className="icon__container">
+        <a href="hhttps://linkedin.com/in/pataki-istván-9908a2196">
+          <img src="https://cdn-icons.flaticon.com/png/512/2504/premium/2504923.png?token=exp=1644802975~hmac=8111e18704984a8278988928815daa3a" alt="icon" className="footer__icon" />
+        </a>
+        <a href="https://github.com/patakiIstvan">
+          <img src="https://cdn-icons.flaticon.com/png/512/2504/premium/2504911.png?token=exp=1644802599~hmac=b1a11f44865f6d43551aa5a7132c4110" alt="icon" className="footer__icon" />
+        </a>
+        <a href="https://www.facebook.com/istvan.pataki.39/">
+          <img src="https://cdn-icons.flaticon.com/png/512/2504/premium/2504903.png?token=exp=1644802937~hmac=3b5f5b10321b65948552bb5a86f3a74d" alt="icon" className="footer__icon" />
+        </a>
+      </div>
       {/* <h2 className="section__title">Contact me</h2>
       <span className="section__subtitle">Get in touch</span>
       <div class="footer__container grid">
